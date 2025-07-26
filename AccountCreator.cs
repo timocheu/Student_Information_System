97b6b8d5d722ef947c3b038a05f85b2c6efa83e5
timocheu <ccs.timotheoche@gmail.com>
2025-07-26 13:35:30 +0800

    Add cryptography argon2, salt hash

--- AccountCreator.cs
+++ AccountCreator.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Student_Information_System.Utilities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,12 @@ namespace Student_Information_System
         public AccountCreator()
         {
             InitializeComponent();
+
+            byte[] salt = Cryptography.GenerateSalt();
+            byte[] hash = Cryptography.HashPassword("12345", salt);
+
+            MessageBox.Show(Convert.ToBase64String(salt));
+            MessageBox.Show(Convert.ToBase64String(hash));
         }
     }
 }
---
+++ Utilities/Cryptography.cs
@@ -0,0 +1,35 @@
+﻿using Konscious.Security.Cryptography;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_Information_System.Utilities
+{
+    public static class Cryptography
+    {
+        public static byte[] GenerateSalt(int length = 16)
+        {
+
+            byte[] salt = new byte[length];
+            RandomNumberGenerator.Fill(salt);
+            return salt;
+        }
+
+        public static byte[] HashPassword(string password, byte[] salt)
+        {
+            using (var argon2 = new Argon2id(Encoding.UTF8.GetBytes(password)))
+            {
+                argon2.Salt = salt;
+                argon2.DegreeOfParallelism = 1; // number of threads
+                argon2.MemorySize = 32768;      // 64 MB
+                argon2.Iterations = 2;          // number of passes
+
+                return argon2.GetBytes(32);     // desired hash length (e.g. 256-bit)
+            }
+        }
+
+    }
+}
