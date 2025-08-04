61e445ed6f80865799d8bbca96bf7a5b6bee377d
timocheu <ccs.timotheoche@gmail.com>
2025-08-05 05:31:41 +0800

    Add font verification

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using Student_Information_System.Forms;
+using System.Drawing.Text;
 
 namespace Student_Information_System
 {
@@ -10,10 +11,25 @@ namespace Student_Information_System
         [STAThread]
         static void Main()
         {
+            // Configurations
+            if (!IsFontExist("Poppins"))
+            {
+                throw new Exception("Error: Unable to find Poppins font");
+            }
+
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new Login());
+        }
+
+        static bool IsFontExist(string fontname)
+        {
+            using (InstalledFontCollection fonts = new())
+            {
+                return fonts.Families
+                    .Any(x => x.Name.Equals(fontname, StringComparison.CurrentCultureIgnoreCase));
+            }
         }
     }
 }
