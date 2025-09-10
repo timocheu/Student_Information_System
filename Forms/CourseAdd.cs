fabba368b7c61421fc0deab8cc17dc9ea7a40fca
timocheu <ccs.timotheoche@gmail.com>
2025-09-10 23:44:18 +0800

    add: load teacher on course adding dgv

--- Forms/CourseAdd.cs
+++ Forms/CourseAdd.cs
@@ -1,12 +1,37 @@
-﻿using ReaLTaiizor.Controls;
+﻿using Microsoft.EntityFrameworkCore;
+using ReaLTaiizor.Controls;
+using Student_Information_System.Models;
 
 namespace Student_Information_System.Forms
 {
     public partial class CourseAdd : Form
     {
+        SisContext db = new();
+
         public CourseAdd()
         {
             InitializeComponent();
+
+            // TODO:
+            // 1. Display teachers
+            loadTeachers();
+            // 2. Make the department of course, same as teacher's deparment
+            // 4. Search for teacher's department or name
+        }
+
+        private void loadTeachers()
+        {
+            dgv_Teachers.DataSource = db.Users
+                .Where(u => u.Role == 2 && u.Teacher != null && u.Teacher.Status == 1)
+                .OrderByDescending(u => u.UserId)
+                .Select(u => new
+                {
+                    u.UserId,
+                    Name = u.FirstName + " " + u.LastName,
+                    u.Teacher!.Department,
+                    u.Teacher.Specialization,
+                })
+                .ToList() ;
         }
 
         // Dispose
@@ -14,6 +39,10 @@ namespace Student_Information_System.Forms
 
         private void btn_ConfirmAdd_Click(object sender, EventArgs e)
         {
+            //
+            // TODO:
+            //
+            // 3. Confirm
             var result = CrownMessageBox.ShowInformation("Is this the correct information?", "Confirm Adding Course", ReaLTaiizor.Enum.Crown.DialogButton.YesNo);
         }
     }
