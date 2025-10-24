643ebd899329331109c5ca20dc841363242ae7b6
timocheu <ccs.timotheoche@gmail.com>
2025-10-24 11:51:02 +0800

    Add: Sis context logger

--- Utilities/SisContenxtLogger.cs
+++ Utilities/SisContenxtLogger.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Student_Information_System.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,62 @@ namespace Student_Information_System.Utilities
 {
     public class SisContenxtLogger
     {
+        private SisContext context;
+
+        private int userId;
+        private string level;
+
+        public SisContenxtLogger(int userId, string level, SisContext context)
+        {
+            this.userId = userId;
+            this.level = level;
+            this.context = context;
+        }
+
+        public void Information(string action, string message)
+        {
+            Logs log = new()
+            {
+                Timestamp = DateTime.Now.ToString(),
+                UserId = userId,
+                Level = level,
+                Action = action,
+                Message = $"[INFORMATION]: {message}",
+            };
+
+            context.Logs.Add(log);
+            context.SaveChangesAsync();
+        }
+
+        public void Warning(string action, string message)
+        {
+            Logs log = new()
+            {
+                Timestamp = DateTime.Now.ToString(),
+                UserId = userId,
+                Level = level,
+                Action = action,
+                Message = $"[WARNING]: {message}",
+            };
+
+            context.Logs.Add(log);
+            context.SaveChangesAsync();
+        }
+
+        public void Error(string action, string message, Exception ex)
+        {
+            Logs log = new()
+            {
+                Timestamp = DateTime.Now.ToString(),
+                UserId = userId,
+                Level = level,
+                Action = action,
+                Message = $"[ERROR]: {message}",
+                Exception = $"{ex.GetType().Name}: {ex.Message}",
+            };
+
+            context.Logs.Add(log);
+            context.SaveChangesAsync();
+        }
     }
 }
